dcc5adccc222245e4eb048a7661fb58f7ff392db
Evgeniy <e.koskov@bk.ru>
2024-11-14 00:08:03 +0500

    finalfunc1

--- main.cpp
+++ main.cpp
@@ -7,24 +7,32 @@ using namespace std;
 
 // Чтение строк из файла в вектор
 void readFromFile(vector<string>& lines) {
-    ifstream in("file1.txt"){
-        if (in.is_open()){
-            while (getline(in, line)){
-                lines.push_back(line);
-            }
-        }
-        in.close()
+    ifstream in("file1.txt")
+
+    string line;
+    while (getline(in, line)){
+        lines.push_back(line);
     }
+
+    in.close();
+
 }
 
-// Функция 2: Вывод строк на экран
+// Вывод строк на экран
 void printLines(const vector<string>& lines) {
-    // Пока оставляем пустой
+    for (const auto& line : lines) {
+        cout << line << endl;
+    }
 }
 
 // Функция 3: Запись строк в файл
 void writeToFile(const vector<string>& lines) {
-    // Пока оставляем пустой
+    ofstream out;
+    out.open("file2.txt");
+    for (const auto& line : lines){
+        out << line << endl;
+    }
+    out.close();
 }
 
 int main() {
