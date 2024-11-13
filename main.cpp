b3652f5b54dbc7ba966c4806a2b64637d89e72d7
Evgeniy <e.koskov@bk.ru>
2024-11-13 22:16:47 +0500

    karkas

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+// Функция 1: Чтение строк из файла в вектор
+void readFromFile(vector<string>& lines) {
+    // Пока оставляем пустой
+}
+
+// Функция 2: Вывод строк на экран
+void printLines(const vector<string>& lines) {
+    // Пока оставляем пустой
+}
+
+// Функция 3: Запись строк в файл
+void writeToFile(const vector<string>& lines) {
+    // Пока оставляем пустой
+}
+
+int main() {
+
+    vector<string> lines;
+
+    readFromFile(lines);
+    printLines(lines);
+    writeToFile(lines);
+
     return 0;
 }
